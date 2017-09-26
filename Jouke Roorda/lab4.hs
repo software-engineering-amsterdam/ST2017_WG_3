31bb70000b85447812a5ad3c3107c038c54504c7
Jouke <26223981+joukero@users.noreply.github.com>
2017-09-26 15:15:22 +0200

    Update exercise 3 of lab 4

--- Jouke Roorda/lab4.hs
+++ Jouke Roorda/lab4.hs
@@ -21,8 +21,13 @@ xs \\\ ys = (filter (flip notElem ys) xs) ++ (filter (flip notElem xs) ys)
 -- Exercise 3 - 20m --
 sUnion :: Ord a => Set a -> Set a -> Set a
 sUnion (Set xs) (Set ys) = Set $ toset $ xs ++ ys
+
 sIntersect :: Ord a => Set a -> Set a -> Set a
 sIntersect (Set xs) (Set ys) = Set $ toset $ xs `intersect` ys
+
 sDifference :: Ord a => Set a -> Set a -> Set a
-sDifference (Set xs) (Set ys) = Set $ toset $ (xs \\\ ys)
+sDifference (Set xs) (Set ys) = Set $ toset $ xs \\ ys
+
+sSymDifference :: Ord a => Set a -> Set a -> Set a
+sSymDifference (Set xs) (Set ys) = Set $ toset $ (xs \\\ ys)
 
