973ec95885953b065ae0b8126e0e2cf82ffcbecc
Edwin <edwin_ou@hotmail.com>
2017-09-26 15:36:53 +0200

    Exercise 3, missing some props

--- Edwin Ouwehand/Lab4/Solutions.hs
+++ Edwin Ouwehand/Lab4/Solutions.hs
@@ -41,14 +41,37 @@ prop_member_delete x s = not (inSet x (deleteSet x s))
 -- use automated testing to check that your implementation is correct. First use your own generator, next use QuickCheck.
 -- (Deliverables: implementations, test properties, short test report, indication of time spent.)
 
-intersection :: Ord a => Set a -> Set a-> Set a
-intersection x y = x
+setIntersection :: Ord a => Set a -> Set a-> Set a
+setIntersection x y = list2set $ intersect (set2List x) (set2List y)
 
-union :: Ord a => Set a -> Set a-> Set a
-union x y = x
+setUnion :: Ord a => Set a -> Set a-> Set a
+setUnion x y = list2set $ sort $ nub $ merge (set2List x) (set2List y)
 
-difference :: Ord a => Set a -> Set a-> Set a
-difference x y = x
+setDifference :: Ord a => Set a -> Set a-> Set a
+setDifference x y = list2set ((set2List x) \\ (set2List y))
+
+setSymDifference :: Ord a => Set a -> Set a-> Set a
+setSymDifference x y = list2set $ merge ((set2List x) \\ (set2List y)) ((set2List y) \\ (set2List x))
+
+-- Merge function 
+-- Src: https://stackoverflow.com/questions/3938438/merging-two-lists-in-haskell
+merge :: [a] -> [a] -> [a]
+merge xs     []     = xs
+merge []     ys     = ys
+merge (x:xs) (y:ys) = x : y : merge xs ys
+
+set2List :: Set a -> [a]
+set2List (Set xs) = xs
+
+prop_union_subset :: Set Int -> Set Int -> Bool
+prop_union_subset x y = subSet x z && subSet y z
+    where z = (setUnion x y)
+
+prop_difference_inset :: Set Int -> Set Int -> Bool
+prop_difference_inset x y = not (any (\ z -> inSet z (setDifference x y)) (set2List y))
+--inSet
+
+--report & timespent 
 
 
 -- Exercise 4
