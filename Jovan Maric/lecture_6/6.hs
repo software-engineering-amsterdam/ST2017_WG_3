f37a5b9b5940ebe5c5241aa0ee4abceac1a785d1
Jovan Maric <jovanmaric777@gmail.com>
2017-10-12 23:55:44 +0200

    Add 6.hs

--- Jovan Maric/lecture_6/6.hs
+++ Jovan Maric/lecture_6/6.hs
@@ -6,3 +6,22 @@ import Data.List
 import System.Random
 import Test.QuickCheck
 import Lecture6
+
+carmichael :: [Integer]
+carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) |
+   k <- [2..],
+   prime (6*k+1),
+   prime (12*k+1),
+   prime (18*k+1) ]
+
+cm_test :: [Integer] -> Int -> IO ()
+cm_test [] k     = print "Nein"
+cm_test (x:xs) k = do
+  p <- primeMR k x
+  if p then
+    print x
+  else
+    cm_test xs k
+
+-- Still quite a lot of false_positives when using primeMR. Even more than with
+-- the test in 5.hs.
